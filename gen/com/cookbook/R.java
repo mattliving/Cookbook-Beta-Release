39b85af4d3441a797b32e2069b48fa51d667a621
giuliom <lefantome@alice.it>
2011-12-09 18:08:38 +0000

    Merged old commit with rob work

--- gen/com/cookbook/R.java
+++ gen/com/cookbook/R.java
@@ -8,6 +8,10 @@
 package com.cookbook;
 
 public final class R {
+    public static final class array {
+        public static final int TypeOfRecipe=0x7f060001;
+        public static final int WeightMeasurement=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -22,18 +26,55 @@ public final class R {
         public static final int sample_7=0x7f020008;
     }
     public static final class id {
-        public static final int gridview=0x7f060000;
+        public static final int AddIngredients=0x7f080009;
+        public static final int AddIngredientsTab=0x7f080000;
+        public static final int Amount1=0x7f080005;
+        public static final int AmountBox0=0x7f080006;
+        public static final int EditButton0=0x7f080004;
+        public static final int IngredientBox0=0x7f080003;
+        public static final int Ingredients1=0x7f080002;
+        public static final int IngredientsTable=0x7f080001;
+        public static final int Measurement1=0x7f080007;
+        public static final int MethodBox=0x7f080015;
+        public static final int NameOfRecipe=0x7f08000c;
+        public static final int RecipeType=0x7f08000d;
+        public static final int RemoveButton0=0x7f080013;
+        public static final int Season=0x7f08000e;
+        public static final int UploadButton=0x7f08000f;
+        public static final int WeightMeasurement0=0x7f080008;
+        public static final int linearLayout1=0x7f080012;
+        public static final int scrollView1=0x7f080011;
+        public static final int tab1=0x7f08000b;
+        public static final int tab2=0x7f080010;
+        public static final int tab3=0x7f080014;
+        public static final int tabHost=0x7f08000a;
     }
     public static final class layout {
-        public static final int list_item=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int recipe_page=0x7f030002;
+        public static final int add_ingredient=0x7f030000;
+        public static final int list_item=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int recipe_page=0x7f030003;
     }
     public static final class raw {
         public static final int recipes=0x7f040000;
     }
     public static final class string {
+        public static final int AddIngredient=0x7f050006;
+        public static final int Amount=0x7f050003;
+        public static final int Edit=0x7f050008;
+        public static final int Ingredient=0x7f050002;
+        public static final int Measurement=0x7f050004;
+        public static final int Method=0x7f050009;
+        public static final int NameOfRecipe=0x7f05000a;
+        public static final int NoMoreIngredients=0x7f050005;
+        public static final int Remove=0x7f05000d;
+        public static final int Seasonal=0x7f05000b;
+        public static final int SubmitIngredients=0x7f050007;
+        public static final int Upload=0x7f05000c;
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
+    }
+    public static final class style {
+        public static final int Measurement=0x7f070000;
     }
 }
--- src/com/cookbook/CookbookActivity.java
+++ src/com/cookbook/CookbookActivity.java
@@ -1,126 +1,321 @@
 package com.cookbook;
 
-import com.cookbook.core.RecipeList;
-
 import android.app.Activity;
-import android.app.ListActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
-//Trying commit and removing cookbook folder
-
-public class CookbookActivity extends ListActivity {
-    
-	/**
-	 * Database Adapter
-	 */
-	private CookBookDbAdapter mDbHelper;
+public class CookbookActivity extends Activity {
+	int counterIngredient = (int) getItemId(R.id.IngredientBox0);
+	int counterAmount = (int) getItemId(R.id.AmountBox0);
+	int counterMeasurement = (int) getItemId(R.id.WeightMeasurement0);
+	int counterEdit = (int) getItemId(R.id.EditButton0);
+	int counterRemove = (int) getItemId(R.id.RemoveButton0);
+	int counterRow = (int) getItemId(R.id.Ingredients1);
+	int remember = (int) 0x7f07007f - (int) getItemId(R.id.RemoveButton0);
 	
-	/** List of Recipes */
-	RecipeList list = new RecipeList();
-
-	/** Called when the activity is first created. */
+	public long getItemId(int position) {  
+	     return position;  
+	} 
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
-        
-        //Initialise the DB
-        mDbHelper = new CookBookDbAdapter(this);
-        mDbHelper.open();
-        createRecipe();
-        createIngredient();
-        createRecipeIngredients();
+        setContentView(R.layout.main);
+        //Initialising variables
+        TableRow rowAmount = (TableRow)findViewById(R.id.Amount1);
+        TableRow rowMeasurement = (TableRow)findViewById(R.id.Measurement1);
+        rowAmount.setId(counterRow+remember);
+        rowMeasurement.setId(counterRow+remember+1);
         
         
-        /*
-         * Add the database entries to the list
-         */
-        list.fetchAllRecipes(mDbHelper);
-        
-        /**
-         * Debugging messages in android!
-         */
-        Log.d("MyDebug", String.valueOf(list.size()));
-        
-        /**
-  	   * adding the list to the recipeArray used to display it
-  	   */
-  	  RECIPES = new String[list.size()];
-  	  for (int i =0; i<list.size();i++){
-  		  RECIPES[i] = list.getRecipe(i).getName()+"\nType: "+list.getRecipe(i).getType();
-  		  System.out.println(list.getRecipe(i).getName());
-  	  }
-        
-        
-     // list_item is in /res/layout/ should be created
-  	  setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, RECIPES));
+        TabHost tabHost=(TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
 
+        TabSpec spec1=tabHost.newTabSpec("Tab 1");
+        spec1.setIndicator("Tab 1",getResources().getDrawable(R.drawable.ic_launcher));
+        spec1.setContent(R.id.tab1);
 
+        TabSpec spec2=tabHost.newTabSpec("Tab 2");
+        spec2.setIndicator("Tab 2",getResources().getDrawable(R.drawable.ic_launcher));
+        spec2.setContent(R.id.tab2);
 
-  	  final ListView lv = getListView();
-  	  lv.setTextFilterEnabled(true);
+        TabSpec spec3=tabHost.newTabSpec("Tab 3");
+        spec3.setIndicator("Tab 3",getResources().getDrawable(R.drawable.ic_launcher));
+        spec3.setContent(R.id.tab3);
 
+        tabHost.addTab(spec1);
+        tabHost.addTab(spec2);
+        tabHost.addTab(spec3);
         
-        
-
-  	/**
-	   * Onclik show the info about the Recipe on a popup message
-	   */
-	  lv.setOnItemClickListener(new OnItemClickListener() {
-	    public void onItemClick(AdapterView<?> parent, View view,
-	        int position, long id) {
-	      // When clicked, show a toast with the TextView text
-	      Toast.makeText(getApplicationContext(), 
-	    "Ingredients: "+list.getRecipe(position).getIngredients()+"\nPreparation: "+list.getRecipe(position).getPreparation()
-	    +"\nType: "+list.getRecipe(position).getType()+"\nRegion: "+list.getRecipe(position).getRegion(),
-	          Toast.LENGTH_SHORT).show();
-	      }
+        Button mainAdd = (Button) findViewById(R.id.AddIngredients);
+        mainAdd.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                EditText Ingredient = (EditText)findViewById(counterIngredient);
+                EditText Amount = (EditText)findViewById(counterAmount);
+                String IngredientString = Ingredient.getText().toString();
+                String AmountString = Amount.getText().toString();
+                if ((IngredientString != null) && (IngredientString.length() > 0)) {
+                	if ((AmountString != null) && (AmountString.length() > 0)) {
+                    	//What to do when ingredient is added completely...
+                		IngredientString = null;
+                		AmountString = null;
+                		EditText IngredientED = (EditText) findViewById(counterIngredient);
+                		EditText AmountED = (EditText) findViewById(counterAmount);
+                		Spinner MeasurementS = (Spinner) findViewById(counterMeasurement);
+                		Button ButtonEdit = (Button) findViewById(counterEdit);
+                		Button ButtonRemove = (Button) findViewById(counterRemove);
+                		IngredientED.setFocusable(false);
+                		AmountED.setFocusable(false);
+                		MeasurementS.setFocusable(false);
+                		MeasurementS.setEnabled(false);
+                		IngredientED.setTextColor(Color.LTGRAY);
+                		AmountED.setTextColor(Color.LTGRAY);
+                		ButtonEdit.setVisibility(View.VISIBLE);
+                		ButtonEdit.setOnClickListener(listenerOfEditButton);
+                		ButtonRemove.setVisibility(View.VISIBLE);
+                		ButtonRemove.setOnClickListener(listenerOfRemoveButton);
+                		counterIngredient += 25;
+                		counterAmount += 25;
+                		counterMeasurement += 25;
+                		counterEdit += 25;
+                		counterRow += 25;
+                		counterRemove += 25;
+                		TableLayout table = (TableLayout) findViewById(R.id.IngredientsTable);
+                		
+                		TableRow rowIngredient = new TableRow(CookbookActivity.this);
+                		EditText tIngredient = new EditText(CookbookActivity.this);
+                		tIngredient.setHint("Ingredient");
+                		tIngredient.setInputType(1);
+                		tIngredient.setId(counterIngredient);
+                		Button editButton = new Button(CookbookActivity.this);
+                		editButton.setVisibility(View.INVISIBLE);
+                		editButton.setText("Edit");
+                		editButton.setId(counterEdit);
+                		editButton.setOnClickListener(listenerOfEditButton);
+                		rowIngredient.setId(counterRow);
+                		rowIngredient.addView(tIngredient);
+                		rowIngredient.addView(editButton);
+                		table.addView(rowIngredient,new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, (float) 1));
+                		
+                		//counterRow += 25;
+                		TableRow rowAmount = new TableRow(CookbookActivity.this);
+                		EditText tAmount = new EditText(CookbookActivity.this);
+                		tAmount.setHint("Amount (weight, volume etc.)");
+                		tAmount.setInputType(0);
+                		tAmount.setId(counterAmount);
+                		Button removeButton = new Button(CookbookActivity.this);
+                		removeButton.setVisibility(View.INVISIBLE);
+                		removeButton.setText("Remove");
+                		removeButton.setId(counterRemove);
+                		removeButton.setOnClickListener(listenerOfRemoveButton);
+                		rowAmount.setId(counterRow+remember);
+                		rowAmount.addView(tAmount);
+                		rowAmount.addView(removeButton);
+                		table.addView(rowAmount,new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, (float) 1));
+                		
+                		//counterRow += 25;
+                		TableRow rowMeasurement = new TableRow(CookbookActivity.this);
+                		Spinner tMeasurement = new Spinner(CookbookActivity.this);
+                		tMeasurement.setPrompt("Measurement");
+                		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                				CookbookActivity.this, R.array.WeightMeasurement, android.R.layout.simple_spinner_item);
+                		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                	    tMeasurement.setAdapter(adapter);
+                		tMeasurement.setId(counterMeasurement);
+                		rowMeasurement.setId(counterRow+remember+1);
+                		rowMeasurement.addView(tMeasurement);
+                		table.addView(rowMeasurement,new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, (float) 1));
+                    } else {
+                    	IngredientString = null;
+                		AmountString = null;
+                    	new AlertDialog.Builder(CookbookActivity.this)
+                        .setTitle("Fill in all fields")
+                        .setMessage("Please enter an amount")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) { 
+                            	dialog.dismiss();
+                            }
+                         })
+                         .show();
+                    }
+                } else {
+                	IngredientString = null;
+            		AmountString = null;
+                	new AlertDialog.Builder(CookbookActivity.this)
+                    .setTitle("Fill in all fields")
+                    .setMessage("Please enter an ingredient")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) { 
+                        	dialog.dismiss();
+                        }
+                     })
+                     .show();
+                }
+            }
         });
     }
-    
-    
-    private void createRecipe() {
-    	mDbHelper.createRecipe("Spaghetti Bolgnaise", "1. Step1\n2. Step2\n" +
-    		"3. Step3\n4. Step4", "Dinner", 30, "", "Italy");
-    	mDbHelper.createRecipe("Mousaka", "1. StepA\n2. StepB\n" +
-        		"3. StepC\n4. StepD", "Lunch", 30, "", "Greek");
-    	mDbHelper.createRecipe("Cheese on Toast", "1. Pre-heat grill\n2. Slice"
-        		+ "cheese\n3. Cook one side of the toast\n4. Turn toast over\n" 
-    			+ "5. Put cheese on uncooked side of toast\n6. Cook until" +
-    			" cheese" +	"is bubbling", "Snack", 10, "", "");
+        
+    private OnClickListener listenerOfEditButton = new OnClickListener()
+    {
+        @Override
+        public void onClick(View v) {
+        	int ID = ((Button)v).getId();
+        	int ItemID = 0;
+        	int difference = 0;
+        	difference = counterIngredient - counterEdit;
+        	ItemID = ID + difference;
+        	EditText editIngredient = (EditText) findViewById(ItemID);
+        	editIngredient.setFocusable(true);
+        	editIngredient.setTextColor(Color.BLACK);
+        	difference = counterAmount - counterEdit;
+        	ItemID = ID + difference;
+        	EditText editAmount = (EditText) findViewById(ItemID);
+        	editAmount.setFocusable(true);
+        	editAmount.setTextColor(Color.BLACK);
+        	difference = counterMeasurement - counterEdit;
+        	ItemID = ID + difference;
+        	Spinner editMeasurement = (Spinner) findViewById(ItemID);
+        	editMeasurement.setFocusable(true);
+        	editMeasurement.setEnabled(true);
+        	Button editToDone = (Button) findViewById(ID);
+        	editToDone.setText("Done");
+        	editToDone.setOnClickListener(listenerOfDoneButton);
+        }
+    };
+    private OnClickListener listenerOfDoneButton = new OnClickListener()
+    {
+        @Override
+        public void onClick(View v) {
+            int ID = ((Button)v).getId();
+        	int IngredientsID = 0;
+        	int AmountID = 0;
+        	int MeasurementID = 0;
+        	int difference = 0;
+        	difference = counterIngredient - counterEdit;
+        	IngredientsID = ID + difference;
+        	difference = counterAmount - counterEdit;
+        	AmountID = ID + difference;
+        	difference = counterMeasurement - counterEdit;
+        	MeasurementID = ID + difference;
+        	EditText Ingredient = (EditText)findViewById(IngredientsID);
+            EditText Amount = (EditText)findViewById(AmountID);
+            String IngredientString = Ingredient.getText().toString();
+            String AmountString = Amount.getText().toString();
+            if ((IngredientString != null) && (IngredientString.length() > 0)) {
+            	if ((AmountString != null) && (AmountString.length() > 0)) {
+                	//What to do when ingredient is added completely...
+            		IngredientString = null;
+            		AmountString = null;
+            		EditText IngredientED = (EditText) findViewById(IngredientsID);
+            		EditText AmountED = (EditText) findViewById(AmountID);
+            		Spinner MeasurementS = (Spinner) findViewById(MeasurementID);
+            		Button ButtonEdit = (Button) findViewById(ID);
+            		IngredientED.setFocusable(false);
+            		AmountED.setFocusable(false);
+            		MeasurementS.setFocusable(false);
+            		MeasurementS.setEnabled(false);
+            		IngredientED.setTextColor(Color.LTGRAY);
+            		AmountED.setTextColor(Color.LTGRAY);
+            		ButtonEdit.setVisibility(View.VISIBLE);
+            		ButtonEdit.setOnClickListener(listenerOfEditButton);
+            		ButtonEdit.setText("Edit");
+                } else {
+                	new AlertDialog.Builder(CookbookActivity.this)
+                    .setTitle("Fill in all fields")
+                    .setMessage("Please enter an amount")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) { 
+                        	dialog.dismiss();
+                        }
+                     })
+                     .show();
+                }
+            } else {
+            	new AlertDialog.Builder(CookbookActivity.this)
+                .setTitle("Fill in all fields")
+                .setMessage("Please enter an ingredient")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) { 
+                    	dialog.dismiss();
+                    }
+                 })
+                 .show();
+            }
+        }
+    };
+    private OnClickListener listenerOfRemoveButton = new OnClickListener()
+    {
+        @Override
+        public void onClick(View v) {
+        	int ID = ((Button)v).getId();
+        	TableLayout table = (TableLayout) findViewById(R.id.IngredientsTable);
+        	int ItemID = 0;
+        	int difference = 0;
+        	difference = counterRow - counterRemove;
+        	ItemID = ID + difference;
+        	TableRow row = (TableRow)findViewById(ItemID);
+        	table.removeView(row);
+        	row = (TableRow)findViewById(ItemID+remember);
+        	table.removeView(row);
+        	row = (TableRow)findViewById(ItemID+remember+1);
+        	table.removeView(row);
+        	
+        }
+    };
+    public void listenerOfUploadButton(View v) {
+    	EditText Ingredient = (EditText)findViewById(counterIngredient);
+        EditText Amount = (EditText)findViewById(counterAmount);
+        EditText Method = (EditText)findViewById(R.id.MethodBox);
+        EditText Name = (EditText)findViewById(R.id.NameOfRecipe);
+        String IngredientString = Ingredient.getText().toString();
+        String AmountString = Amount.getText().toString();
+        String MethodString = Method.getText().toString();
+        String NameString = Name.getText().toString();
+        if ((IngredientString != null) && (IngredientString.length() > 0) &&
+        		(AmountString != null) && (AmountString.length() > 0) &&
+        		(MethodString != null) && (MethodString.length() > 0) &&
+        		(NameString != null) && (NameString.length() > 0)) {
+        	new AlertDialog.Builder(CookbookActivity.this)
+            .setTitle("Upload Recipe?")
+            .setMessage("Are you sure you want to upload recipe: "+ NameString)
+            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                	uploadInformation();
+                	dialog.dismiss();
+                }
+             })
+             .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) { 
+                	dialog.dismiss();
+                }
+             })
+             .show();
+        } else {
+        	new AlertDialog.Builder(CookbookActivity.this)
+        	.setTitle("Fill in all fields")
+        	.setMessage("Please fill in all recipe details")
+        	.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+        		public void onClick(DialogInterface dialog, int which) { 
+        			dialog.dismiss();
+        		}
+        	})
+        	.show();
+        }
     }
-
-    private void createRecipeIngredients() {
-    	mDbHelper.createRecipeIngredient(1, 1, 6, "Qty");
-    	mDbHelper.createRecipeIngredient(1, 2, 150, "g");
-    	mDbHelper.createRecipeIngredient(1, 3, 300, "g");
-    	mDbHelper.createRecipeIngredient(2, 1, 200, "g");
-    	mDbHelper.createRecipeIngredient(2, 1, 200, "g");
-    	mDbHelper.createRecipeIngredient(3, 1, 30, "g");
-    	mDbHelper.createRecipeIngredient(3, 2, 3, "g");
+    public void uploadInformation() {
+    	//upload shit!
     }
-
-    private void createIngredient() {
-    	mDbHelper.createIngredient("Tomatoes");
-    	mDbHelper.createIngredient("Pasta");
-    	mDbHelper.createIngredient("Beef");
-    	mDbHelper.createIngredient("Lamb");
-    	mDbHelper.createIngredient("Cheese");
-    	mDbHelper.createIngredient("Bread");
-    }
-    
-    
-    /** need it*/
-	 String[] RECIPES = new String[]{"lol"};
-
-    
-    
 }
